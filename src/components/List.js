73d877272224a54db22191b0f0a00c16a5f508e4
Jarrod-Payton <jarrodlpayton@gmail.com>
2023-08-02 18:47:42 -0500

    Tailwind installed and setup

--- src/components/List.js
+++ src/components/List.js
@@ -1,24 +1,41 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 // React Conditional Rendering Ladies and Gentlmen (I think)
-function DeleteButton({ show, deleteList }) {
+function Edits({ show, deleteList }) {
   if (show) {
-    return <button onClick={deleteList}>X</button>;
+    return (
+      <div className="flex justify-between px-2 shadow-sm">
+        <button className="text-sm font-bold">Edit</button>
+        <button className="hover:text-red-500 font-bold" onClick={deleteList}>
+          X
+        </button>
+      </div>
+    );
   } else if (!show) {
     return null;
   }
 }
 
 export default function List({ list, deleteListById, allowListEditing }) {
+  let navigate = useNavigate();
+
   async function deleteList() {
     deleteListById(list.id);
   }
+
+  async function buttonClicked(e) {
+    navigate(`/list/${list.id}`);
+  }
+
   return (
-    <div>
-      <label>
-        <Link to={`/list/${list.id}`}>{list.name}</Link>
-      </label>
-      <DeleteButton show={allowListEditing} deleteList={deleteList} />
+    <div className="my-3 mx-1">
+      <button
+        onClick={buttonClicked}
+        className="p-2 outline outline-1 shadow-lg rounded-md w-full text-lg font-mono hover:bg-gray-100"
+      >
+        {list.name}
+      </button>
+      <Edits show={allowListEditing} deleteList={deleteList} />
     </div>
   );
 }
